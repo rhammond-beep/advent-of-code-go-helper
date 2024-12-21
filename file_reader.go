81947ff97864d644f6455425f999344c54bb942e
Rupert Hammond <ruperthammond@costellomedical.com>
2024-12-21 17:22:30 +0000

    added documentation for existing method and added new option for file reading

--- file_reader.go
+++ file_reader.go
@@ -3,8 +3,13 @@ package util
 import (
 	"bufio"
 	"os"
+	"strings"
 )
 
+/*
+Reads file line by line, converting each as a string and appending
+to return slice
+*/
 func ReadChallengeInput(filepath string) (fileContents []string) {
 	file, err := os.Open(filepath)
 	if err != nil {
@@ -17,4 +22,22 @@ func ReadChallengeInput(filepath string) (fileContents []string) {
 		fileContents = append(fileContents, scanner.Text())
 	}
 	return
+}
+
+/*
+Reads entire UTF-8 encoded file as a one contiguous string
+*/
+func ReadChallengeInputContigious(filepath string) string {
+	file, err := os.Open(filepath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var sb strings.Builder
+	for scanner.Scan() {
+		sb.WriteString(scanner.Text())
+	}
+	return sb.String()
 }
